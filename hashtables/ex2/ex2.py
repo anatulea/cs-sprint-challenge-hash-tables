fdd11c94c89b0b7076322956c73fc992a0bae6f9
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-11-07 10:28:31 -0800

    reconstruct_trip done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,15 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    hash_table = {} # Create a hash table to store key: source, value: destination
+    route = [None]* length # initiate an list to store the itinerary
+
+    for fly in tickets: # loop over the tickets list
+        hash_table[fly.source] = fly.destination # create a key-value  pair for each item in the list
+    next_fly = hash_table['NONE'] # Save the first flight as the next flight variable
+
+    for curr_fly in range(0,length): # Add the flights in proper order in the route list
+        route[curr_fly]= next_fly # set the first element in the rout list to be the first flight
+        next_fly = hash_table[next_fly] # set the next flight to be equal to the destination at the hash_table with the next_flight key
 
     return route
